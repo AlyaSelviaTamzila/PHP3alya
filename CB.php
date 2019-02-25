419e8ff44d11d2b3e7d66d717e64ed06b2865b7f
AlyaSelviaTamzila <42310789+AlyaSelviaTamzila@users.noreply.github.com>
2019-02-26 06:47:37 +0700

    Add files via upload

---
+++ CB.php
@@ -0,0 +1,9 @@
+<?php 
+    $email = "alyatamzila@gmail.com";
+    $password = "12345";
+    if ($email == "alyatamzila@gmail.com" && $password == "12345"){
+        echo "Login Berhasil";
+    } else{
+        echo "Login Gagal";
+    }
+?>
---
+++ TUGAS (2).php
@@ -0,0 +1,28 @@
+<head>
+    <title>Desain Form Login Dengan Css</title>
+    <link rel="stylesheet" href="TUGAS.css">
+</head>
+<body>
+    <div class="konten">
+        <div class="kepala">
+            <div class="lock"></div>
+            <h2 class="judul">Sign In</h2>
+        </div>
+        <div class="artikel">
+            <form action="CB.php" method="post">
+                <div class="grup">
+                    <label for="email" name="email">E-mail Address</label>
+                    <input type="text" placeholder="Masukkan Alamat Email Anda">
+                </div>
+                <div class="grup">
+                    <label for="password" name="password">Password</label>
+                    <input type="password" placeholder="Masukkan password Anda">
+                </div>
+                <div class="grup">
+                    <input type="submit" value="Sign In">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
